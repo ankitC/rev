b37cda34570286c9f0dcf19035b61b753c6b4cb0
Ankit <ankit.ac.chheda@gmail.com>
2014-01-15 01:12:24 -0500

    Adding the node to the nth location

--- linked_lists/linkedlist.c
+++ linked_lists/linkedlist.c
@@ -1,8 +1,11 @@
 #include "linkedlist.h"
+#include <assert.h>
 
 int Count(struct node* head, int search_for);
 int get_nth(struct node* head, int position);
 void delete_list(struct node** head);
+int pop(struct node** headRef);
+void insert_nth(struct node** headRef, int pos, int element);
 
 void main()
 {
@@ -11,8 +14,20 @@ void main()
 //	int count = Count(head, 2);
 //	int count = get_nth(head, 0);
 //	printf("Count is: %d\n", count);
-	delete_list(&head);
-	print_list(head);
+//	delete_list(&head);
+//	print_list(head);
+//	int i = 0;
+//	int length = list_length(head);
+/*	for(i = 0; i<length; i++)
+	{
+		int c = pop(&head);
+		printf("%d\t", c);
+	}
+	printf("\n");
+*/
+//	insert_nth(&head,0,10);
+//	print_list(head);
+
 }
 
 int Count(struct  node* head, int search_for)
@@ -62,4 +77,42 @@ void delete_list(struct node** head)
 	}
 	*head = NULL;
 	return;
+}
+
+int pop(struct node** headRef)
+{
+	struct node* head = *headRef;
+	if(head == NULL)
+		return 0;
+	int retval = head->data;
+	*headRef = (*headRef)->next;
+	free(head);
+	return retval;
+}
+
+void insert_nth(struct node** headRef, int pos, int element)
+{
+	struct node* head = *headRef;
+	struct node* current = head;
+	int i = 0;
+	if(pos > (list_length(head) + 1))
+	{
+		printf("Trying to insert at invalid position!\n");
+		return;
+	}
+
+	struct node* new_node = (struct node*) malloc(sizeof(struct node));
+	new_node->data = element;
+	if(pos == 0)
+	{
+		new_node->next = head;
+		*headRef = new_node;
+		return;
+	}
+
+	for(i=0; i<pos-1; i++)
+		current = current->next;
+
+	new_node->next = current->next;
+	current->next = new_node;
 }
