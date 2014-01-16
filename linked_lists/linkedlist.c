8990a5927c5ffdeddaff88f9d5d67e1f682ebc9c
Ankit <ankit.ac.chheda@gmail.com>
2014-01-15 22:16:28 -0500

    Adding sorting methods

--- linked_lists/linkedlist.c
+++ linked_lists/linkedlist.c
@@ -6,6 +6,8 @@ int get_nth(struct node* head, int position);
 void delete_list(struct node** head);
 int pop(struct node** headRef);
 void insert_nth(struct node** headRef, int pos, int element);
+void sorted_insert(struct node** headRef, struct node* new_node);
+void insert_sort(struct node** headRef);
 
 void main()
 {
@@ -27,6 +29,38 @@ void main()
 */
 //	insert_nth(&head,0,10);
 //	print_list(head);
+#ifdef SORTED_INSERT
+	struct node* new_node1 = (struct node*) malloc(sizeof(struct node));
+	new_node1->data = 13;
+	sorted_insert(&head, new_node1);
+	print_list(head);
+	struct node* new_node2 = (struct node*) malloc(sizeof(struct node));
+	new_node2->data = 0;
+	sorted_insert(&head, new_node2);
+	print_list(head);
+	struct node* new_node3 = (struct node*) malloc(sizeof(struct node));
+	new_node3->data = 8;
+	sorted_insert(&head, new_node3);
+	print_list(head);
+	struct node* new_node4 = (struct node*) malloc(sizeof(struct node));
+	new_node4->data = 4;
+	sorted_insert(&head, new_node4);
+	print_list(head);
+	struct node* new_node5 = (struct node*) malloc(sizeof(struct node));
+	new_node5->data = 30;
+	sorted_insert(&head, new_node5);
+	print_list(head);
+#endif
+
+	insert_nth(&head,0,10);
+	insert_nth(&head,1,20);
+	insert_nth(&head,3,14);
+	insert_nth(&head,4,78);
+	insert_nth(&head,1,32);
+	insert_nth(&head,3,42);
+	print_list(head);
+	insert_sort(&head);
+	print_list(head);
 
 }
 
@@ -115,4 +149,62 @@ void insert_nth(struct node** headRef, int pos, int element)
 
 	new_node->next = current->next;
 	current->next = new_node;
+}
+
+void sorted_insert(struct node** headRef, struct node* new_node)
+{
+	struct node* current = *headRef;
+	struct node* previous = NULL;
+
+	/* Empty List */
+	if(current == NULL)
+	{
+		new_node->next == NULL;
+		*headRef = new_node;
+		return;
+	}
+
+	/* Insert at the head */
+	if(new_node->data <= current->data)
+	{
+		new_node->next = current;
+		*headRef = new_node;
+		return;
+	}
+
+	while((current->data <=new_node->data)&&current->next!=NULL)
+	{
+		previous = current;
+		current = current->next;
+	}
+
+	/* Insert at the tail */
+	if(current->next == NULL && (current->data < new_node->data))
+	{
+		current->next = new_node;
+		new_node->next = NULL;
+		return;
+	}
+
+	/* Insert in between */
+	previous->next = new_node;
+	new_node->next = current;
+	return;
+}
+
+
+void insert_sort(struct node** headRef)
+{
+	struct node* head = *headRef;
+	struct node* new_head = NULL;
+	struct node* next_head = NULL;
+	while(head != NULL)
+	{
+		next_head = head->next;
+		head->next = NULL;
+		sorted_insert(&new_head, head);
+		head = next_head;
+	}
+
+	*headRef = new_head;
 }
